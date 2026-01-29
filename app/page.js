2d6570a97b3bd63820a66c49c37412a4473e6d30
aditya maurya <aditya2018m@gmail.com>
2026-01-29 17:44:37 +0530

    completed

--- app/page.js
+++ app/page.js
@@ -1,7 +1,59 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState, useMemo, useCallback } from "react";
+import axios from "axios";
 
-const page = () => {
-  return <div>test</div>;
+const Page = () => {
+  const url = "https://jsonplaceholder.typicode.com/users";
+
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [search, setSearch] = useState("");
+
+  const fetchData = useCallback(async () => {
+    setLoading(true);
+    setError("");
+    try {
+      const res = await axios.get(url);
+      setUsers(res.data);
+    } catch {
+      setError("Failed to fetch users");
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const filteredUsers = useMemo(() => {
+    return users.filter((user) =>
+      user.name.toLowerCase().includes(search.toLowerCase())
+    );
+  }, [users, search]);
+
+  return (
+    <div className="p-6 border w-6/12 h-full">
+      <h2>User List</h2>
+
+      <input
+        className="border text-[14px] outline-none p-1"
+        type="text"
+        placeholder="Search user..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      {loading && <p>Loading...</p>}
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      <ul>
+        {filteredUsers.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
-export default page;
+export default Page;
